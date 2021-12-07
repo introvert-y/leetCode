844a5aaf6667f359f6a4a5f849c078aa28464aec
wuxichang <wuxichang@zuzuche.com>
2021-12-08 07:25:18 +0800

    补充

--- 链表/91. 解码方法.js
+++ 链表/91. 解码方法.js
@@ -28,6 +28,29 @@ var numDecodings = function (s) {
   return f[len];
 };
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var numDecodings = function(s) {
+  const len = s.length;
+  // a = f[i - 2]
+  // b = f[i - 1]
+  // c = f[i]
+  let a = 0, b = 1, c = 0;
+  for (let i = 1; i <= len; i++) {
+      c = 0;
+      if (s[i - 1] != 0) {
+          c += b;
+      }
+      if (i > 1 && s[i - 2] !=0 && (s[i - 2] - 0) * 10 + (s[i - 1] - 0) <= 26) {
+          c += a;
+      }
+      a = b;
+      b = c;
+  }
+  return c
+};
 
 /**
  * @param {string} s
